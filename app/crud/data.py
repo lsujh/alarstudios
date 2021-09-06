0aede2f7bea7fc5a61a865e313b9d65e61e94554
ihor.diakiv <ihor.diakiv@lanars.com>
2021-09-06 16:56:50 +0300

    upload data withot database

--- app/crud/data.py
+++ app/crud/data.py
@@ -1,3 +1,7 @@
+import asyncio
+import json
+
+import aiofiles
 from fastapi import File
 from sqlalchemy.orm import Session
 
@@ -12,4 +16,29 @@ def add_data(content: File, db: Session) -> None:
         db.add_all(datas)
         db.commit()
     except:
+        pass
+
+
+async def read_file(file_name):
+    async with aiofiles.open(file_name, mode='r') as f:
+        content = await f.read()
+        return json.loads(content)
+
+
+def list_merge(_contents) -> list:
+    contents = []
+    for content in _contents:
+        contents.extend(content)
+    return sorted(contents, key=lambda content: content['id'])
+
+
+def read_files_async(file_names: list) -> list:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        contents = loop.run_until_complete(asyncio.wait_for(
+            asyncio.gather(*[read_file(file_name) for file_name in file_names],
+                           return_exceptions=True), timeout=2.0))
+        return list_merge(contents)
+    except asyncio.TimeoutError:
         pass
--- app/views/data/data.py
+++ app/views/data/data.py
@@ -1,16 +1,13 @@
-import asyncio
 import json
 
-import aiofiles
 from fastapi import APIRouter, Depends, File, HTTPException, Request, UploadFile
 from sqlalchemy import text
 from sqlalchemy.orm import Session
-from starlette.responses import RedirectResponse
 from starlette.status import HTTP_400_BAD_REQUEST
 from starlette.templating import Jinja2Templates
 
 from app.constants import BASE_PATH
-from app.crud.data import add_data
+from app.crud.data import add_data, read_files_async
 from app.db.session import get_db
 
 router = APIRouter()
@@ -39,16 +36,9 @@ async def list_data(request: Request, db: Session = Depends(get_db)):
 
 
 @router.post("/upload-data/")
-async def upload_data(request: Request, db: Session = Depends(get_db)):
+def upload_data(request: Request):
     files = ['/usr/src/source_1.json', '/usr/src/source_2.json', '/usr/src/source_3.json']
-    for file in files:
-        async with aiofiles.open(file, 'r') as f:
-            content = await f.read()
-        try:
-            await asyncio.wait_for(aiofiles.open(file, 'r'), timeout=2)
-            content = json.loads(content)
-            add_data(content, db)
-        except asyncio.TimeoutError:
-            continue
-
-    return RedirectResponse(request.url_for('list_data'), status_code=302)
+    contents = read_files_async(files)
+    if not contents:
+        contents = {}
+    return templates.TemplateResponse("data/list-data.html", {"request": request, "datas": contents})
